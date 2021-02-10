1683677c58300adeb63365073ebf2fd7f66e47da
exodojaffar <exodojaffar@gmail.com>
2021-02-10 17:47:35 -0400

    update: implemente a classe Resposta

--- Question.java
+++ Question.java
@@ -1,34 +1,31 @@
 import java.util.ArrayList;
+
 public class Question {
 	private String pergunta;
-	private ArrayList<String> altenativas = new ArrayList<String>();
-	private int alternativaCerta = -1;
+	private ArrayList<Resposta> altenativas = new ArrayList<Resposta>();
 
-	public void addAlternativa(String altenativa, boolean essaCorreta){
+	public void addAlternativa(String textoResposta, boolean essaCorreta){
+		Resposta altenativa = new Resposta(textoResposta);
+		altenativa.setCerta(essaCorreta);
+
 		if (this.altenativas.size() < 5){
-			this.altenativas.add(altenativa);
-
-			if(essaCorreta){
-				this.alternativaCerta = this.altenativas.size() -1;
-			}
+			altenativas.add(altenativa)
 		}
 	}
-	
+
 	public String getPergunta(){
 		return this.pergunta;
 	}
+	
 	public void setPergunta(String pergunta){
 		this.pergunta = pergunta;
 	}
+
+	// Mudar
 	public ArrayList<String> getAlternativas(){
 		return this.altenativas;
 	}
 	public boolean verificaResposta(int altenativaUser){
-		if (this.alternativaCerta == altenativaUser) {
-			return true;
-		}
-		else {
-			return false;
-		}
+		return this.altenativas.get(altenativaUser).isCerta();
 	}
 }
