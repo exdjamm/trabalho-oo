0063d2714e72e146ba2f1b127ce73f294d994734
exodojaffar <exodojaffar@gmail.com>
2021-02-10 17:47:18 -0400

    feat: Tipo resposta

---
+++ Resposta.java
@@ -0,0 +1,16 @@
+class Resposta{
+	private String texto;
+	private boolean certa = false;
+
+	public Resposta(String texto){
+		this.texto = texto;
+	}
+
+	public void setCerta(boolean certa){
+		this.certa = certa;
+	}
+
+	public boolean isCerta(){
+		return this.certa;
+	}
+}
